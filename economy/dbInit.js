b148d572db4d5a003b255dd21bd88b2e8869ad4d
jairus1435 <greatlyrejoice@gmail.com>
2019-07-10 17:22:30 -0700

    git add . && git commit -m 'Initial commit'

--- economy/dbInit.js
+++ economy/dbInit.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
-	operatorsAliases: false,
+	operatorsAliases: true,
 });
 
 const CurrencyShop = sequelize.import('CurrencyShop');
