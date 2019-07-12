55334c541b3d211ab5efb5c397d3633bc7507eec
Heroku-Dev-98 <52804844+Heroku-Dev-98@users.noreply.github.com>
2019-07-12 11:54:58 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.prefix = 'process.env.prefix || >';
+exports.token = 'process.env.token';
+exports.DefaultColor = 'process.env.DefaultColor || 0x0094FF';
+exports.avatarURL = 'process.env.avatarURL';
+exports.GOOGLE_API_KEY = 'process.env.GOOGLE_API_KEY || AIzaSyC4KSDwjrkXxiUGp5HT_SNNLLpVExIyt8I';
+exports.FORTNITE_API_KEY = '';
