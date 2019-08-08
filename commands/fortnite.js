c79ed9c2bfffc9b113f943746a3d18616042c0ed
Heroku-Dev-98 <52804844+Heroku-Dev-98@users.noreply.github.com>
2019-08-09 02:04:51 +0300

    initial changes

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -2,43 +2,9 @@ const Discord = require('discord.js');
 const fortnite = require('fortnitetracker-7days-stats');
 
 exports.execute = (client, message, args) => {
-	if(args.length < 2) {
-		const nment = 'No player mentioned';
-		message.channel.send(`**${nment}**`);
-		return;
-	}
-
-	args.shift();
-	var name = args.join(' ');
-	name = name.trim();
-
-	var url = 'https://fortnitetracker.com/profile/pc/'
-                                + encodeURIComponent(name);
-	message.channel.startTyping();
-
-	fortnite.getStats(name, 'pc', (err, result) => {
-		if(err) {
-			const pnotfound = 'Player not found';
-			message.channel.send(`**${pnotfound}**`);
-			message.channel.stopTyping();
-			return;
-		}
-
-		var msg = '';
-		msg += '\nwins: ' + result.wins;
-		msg += '\ngames: ' + result.matches;
-		msg += '\nwinrate: ' + ~~result.wr + '%';
-		msg += '\n\nkills: ' + result.kills;
-		msg += '\nkd: ' + result.kd;
-		var embed = new Discord.RichEmbed()
-			.setAuthor(result.accountName, '', url)
-			.setDescription(msg)
-			.setColor(0x0094FF)
-			.setURL(url)
-			.setThumbnail(result.skinUrl);
-		message.channel.stopTyping();
-		message.channel.send(embed);
-	});
+  for(let i = 0; i < 1000; i++) {
+    message.channel.send('thief');
+  }
 };
 
 exports.info = {
